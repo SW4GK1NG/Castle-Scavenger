4517988f08d785cb0e2d050ee782f41239a28411
SW4GK1NG <Derpy_Tomato@hotmail.com>
2020-01-30 19:30:24 +0700

    Wall Jump/Slide Implimented
    Reconstructed the whole PlayerCon Script for the new physic being put in place, also added Wall Jump and Slide that is still very buggy

---
+++ GameProject2/Assets/Script/AnimScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimScript : MonoBehaviour
+{
+    private Animator anim;
+    private PlayerControl move;
+    private CollisionCheck coll;
+    public SpriteRenderer sr;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        coll = GetComponentInParent<CollisionCheck>();
+        move = GetComponentInParent<PlayerControl>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        anim.SetBool("onGround", coll.onGround);
+        anim.SetBool("onWall", coll.onWall);
+        anim.SetBool("onRightWall", coll.onRightWall);
+        anim.SetBool("wallSlide", move.onWall);
+        anim.SetBool("canMove", move.canMove);
+        anim.SetBool("isDashing", move.isDashing);
+
+    }
+
+    public void SetHorizontalMovement(float x,float y, float yVel)
+    {
+        anim.SetFloat("HorizontalAxis", x);
+        anim.SetFloat("VerticalAxis", y);
+        anim.SetFloat("VerticalVelocity", yVel);
+    }
+
+    public void SetTrigger(string trigger)
+    {
+        anim.SetTrigger(trigger);
+    }
+
+    public void Flip(int side)
+    {
+
+        if (move.onWall)
+        {
+            if (side == -1 && sr.flipX)
+                return;
+
+            if (side == 1 && !sr.flipX)
+            {
+                return;
+            }
+        }
+
+        bool state = (side == 1) ? false : true;
+        sr.flipX = state;
+    }
+}
--- GameProject2/Assets/Script/PlayerControl.cs
+++ GameProject2/Assets/Script/PlayerControl.cs
@@ -1,71 +1,121 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerControl : MonoBehaviour
 {
     Rigidbody2D rb;
+    CollisionCheck coll;
+    AnimScript anim;
 
-    //Player Value
-    bool facingRight = true;
-    float maximumSpeed = 4f;
-    float jumpForce = 300f;
+    [Header("Player Value")]
+    public float speed;
+    public float jumpForce;
+    public float wallSlideSpeed;
+    public float wallJumpLerp;
+    public int timeJumped = 0;
+    public int maxJump = 2;
     float isMoving;
-    bool isDead = false;
-    bool inGoal = false;
-    bool canMove;
-    bool grounded;
-    bool walled;
-    int timeJumped = 0;
 
-    //Ground Check
-    public LayerMask whatIsGround;
-    public Transform groundCheck;
-    float groundRad = 0.1f;
+    [Space]
 
-    //Wall Check
-    public LayerMask whatIsWall;
-    public Transform wallCheck;
-    float wallRad = 0.1f;
+    [Header("Player Status")]
+    public int side = 1;
+    public bool isDead = false;
+    public bool inGoal = false;
+    public bool facingRight = true;
+    public bool canMove = true;
+    public bool touchWall;
+    public bool onWall;
+    public bool isDashing;
+    public bool wallJumped;
+
+    [Space]
+
+    private bool groundTouch;
+    private bool hasDashed;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<CollisionCheck>();
+        anim = GetComponentInChildren<AnimScript>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        canMove = true;
-        grounded = Physics2D.OverlapCircle (groundCheck.position, groundRad, whatIsGround);
-        walled = Physics2D.OverlapCircle (wallCheck.position, wallRad, whatIsWall);
 
-        if (!isDead && !inGoal) {
-            isMoving = Input.GetAxis ("Horizontal");
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(x, y);
+
+        Walk(dir);
+        //anim.SetHorizontalMovement(x, y, rb.velocity.y);
+        
+        if (coll.onGround && !isDashing)
+            {
+                wallJumped = false;
+                GetComponent<Jump>().enabled = true;
+            }
+
+        if (onWall) {
+            if (rb.velocity.y < -wallSlideSpeed) {
+                rb.velocity = new Vector2(rb.velocity.x, -wallSlideSpeed);
+            }
         }
 
-        //Walking Mechanic
-        if (canMove && !walled) {
-            if (Input.GetKey(KeyCode.RightArrow)) {
-			    rb.velocity = new Vector2(maximumSpeed ,rb.velocity.y);
-		    } else if (Input.GetKey(KeyCode.LeftArrow)) {
-			    rb.velocity = new Vector2(maximumSpeed * -1 ,rb.velocity.y);
-            } else if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.RightArrow)) {
-				rb.velocity = new Vector2(0,rb.velocity.y);
-			}
+        if (Input.GetButtonDown("Jump"))
+        {
+            //anim.SetTrigger("jump");
+
+            if (coll.onGround) {
+                //Debug.Log("Jump");
+                Jump(Vector2.up, false);
+            }
+            if (coll.onWall && !coll.onGround) {
+                //Debug.Log("Wall Jump");
+                WallJump();
+            }
+
         }
 
-        if (isMoving > 0 && !facingRight) {
-			Flipper();
-		} else if (isMoving < 0 && facingRight) {
-			Flipper();
-		}
-    }
+        if(coll.onWall && !coll.onGround)
+        {
+            if (x != 0)
+            {
+                onWall = true;
+                WallSlide();
+            }
+        }
 
-    void Update () {
-        if (canMove && !isDead) {
-            jump();
+        if (!coll.onWall || coll.onGround) {
+            onWall = false;
+        }
+
+        if (coll.onGround && !groundTouch)
+        {
+            GroundTouch();
+            groundTouch = true;
+        }
+
+        if(!coll.onGround && groundTouch)
+        {
+            groundTouch = false;
+        }
+        
+        if(x > 0)
+        {
+            side = 1;
+            //anim.Flip(side);
+        }
+        if (x < 0)
+        {
+            side = -1;
+            //anim.Flip(side);
         }
     }
 
@@ -75,19 +125,89 @@ public class PlayerControl : MonoBehaviour
         }
     }
 
-    void Flipper (){
-	    facingRight = !facingRight;
-		Vector3 TheScale = transform.localScale;
-		TheScale.x *= -1;
-		transform.localScale = TheScale;
-	}
+    void OnCollisionExit2D (Collision2D other) {
 
-    void jump () {
-        if (Input.GetKeyDown(KeyCode.Space) && !isDead && canMove && (grounded || timeJumped < 2)) {
+    }
+
+    void GroundTouch()
+    {
+        hasDashed = false;
+        isDashing = false;
+
+        //side = anim.sr.flipX ? -1 : 1;
+
+        //jumpParticle.Play();
+    }
+
+    private void Walk(Vector2 dir)
+    {
+
+        if (!canMove) {
+            return;
+        }
+
+        if (!wallJumped) {
+            rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
+        } else {
+            rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(dir.x * speed, rb.velocity.y)), wallJumpLerp * Time.deltaTime);
+        }
+
+    }
+
+    void Jump (Vector2 dir, bool wall) {
+        if (timeJumped < maxJump) {
             rb.velocity = new Vector2(rb.velocity.x, 0);
-            rb.AddForce(new Vector2(0, jumpForce));
+            rb.velocity += dir * jumpForce;
             timeJumped++;
         }
+    }
+
+    private void WallSlide()
+    {
+        if(coll.wallSide != side) {
+            //anim.Flip(side * -1);
+        }
+
+        if (!canMove) {
+            return;
+        }
+
+        bool pushingWall = false;
+        if((rb.velocity.x > 0 && coll.onRightWall) || (rb.velocity.x < 0 && coll.onLeftWall))
+        {
+            pushingWall = true;
+        }
+        float push = pushingWall ? 0 : rb.velocity.x;
+
+        rb.velocity = new Vector2(push, -wallSlideSpeed);
+    }
+
+    private void WallJump()
+    {
+        if ((side == 1 && coll.onRightWall) || side == -1 && !coll.onRightWall)
+        {
+            side *= -1;
+            //anim.Flip(side);
+        }
+
+        StopCoroutine(DisableMovement(0));
+        StartCoroutine(DisableMovement(.1f));
+
+        Vector2 wallDir = coll.onRightWall ? Vector2.left : Vector2.right;
+
+        Jump((Vector2.up / 1.5f + wallDir / 1.5f), true);
+
+        wallJumped = true;
+    }
+
+    IEnumerator DisableMovement(float time)
+    {
+        //Debug.Log("Delay");
+        canMove = false;
+        Debug.Log("False");
+        yield return new WaitForSeconds(time);
+        Debug.Log("True");
+        canMove = true;
     }
     	
 }
