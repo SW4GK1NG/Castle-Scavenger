2361c822fafec9e295a768f9583a677316f38f73
SW4GK1NG <Derpy_Tomato@hotmail.com>
2020-02-16 16:22:46 +0700

    Swing Axe added, Slammer on the work
    Swinging Axe was added, but really could use some improvement, Slammer is getting make and will probably take 100% of my brain power

---
+++ GameProject2/Assets/Script/Slammer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slammer : MonoBehaviour
+{
+
+    [Header("Layers")]
+    public LayerMask groundLayer;
+    public LayerMask playerLayer;
+
+    [Space]
+
+    public bool onGround;
+    public bool Detected;
+    public  float downVelocity;
+
+    [Space]
+
+    [Header("Detection Area")]
+
+    public Vector2 collisionSize;
+    public Vector2 detectionSize;
+    private Color debugCollisionColor = Color.red;
+
+    [Header("Offset")]
+
+    public Vector2 detectionOffset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        detectionOffset = new Vector2(0, detectionSize.y);
+
+        onGround = Physics2D.OverlapBox((Vector2)transform.position, collisionSize, groundLayer);
+        Detected = Physics2D.OverlapBox((Vector2)transform.position - detectionOffset, detectionSize, playerLayer);
+    }
+
+    void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube((Vector2)transform.position, collisionSize);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube((Vector2)transform.position - detectionOffset, detectionSize);
+    }
+}
---
+++ GameProject2/Assets/Script/SwingAxe.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingAxe : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float LeftRange;
+    public float RightRange;
+    public float MaxVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.angularVelocity = MaxVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        push();
+    }
+
+    void push () {
+        if (transform.rotation.z > 0 && transform.rotation.z < RightRange && rb.angularVelocity > 0 && rb.angularVelocity < MaxVelocity) {
+            rb.angularVelocity = MaxVelocity;
+            //Debug.Log("Right");
+        } else if (transform.rotation.z < 0 && transform.rotation.z > LeftRange && rb.angularVelocity < 0 && rb.angularVelocity > MaxVelocity * -1) {
+            rb.angularVelocity = MaxVelocity * -1;
+            //Debug.Log("Left");
+        }
+    }
+
+}
